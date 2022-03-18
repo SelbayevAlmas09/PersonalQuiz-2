0d6c3cd3cba00002209c5fe2745b9c7878c620d2
Селбаев Алмас <almasselbayev@gmail.com>
2022-03-18 09:30:18 +0600

    Домашка с учетом замечании

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -11,25 +11,31 @@ class ResultViewController: UIViewController {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
     
-    @IBOutlet var backNavigationItem: UINavigationItem!
-    
     var answersChosen: [Answer]!
-    var animalsCount: [Animal: Int] = [.dog : 0, .cat : 0, .rabbit : 0, .turtle : 0]
+    
+    private var animalsCount: [Animal: Int] = [:]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
         
-        backNavigationItem.hidesBackButton = true
-        
-        let animals = answersChosen.map {$0.animal}
-        
-        for animal in animals {
-            animalsCount[animal]! += 1
-        }
-        
-        let resultedAnimal = animalsCount.sorted {$0.1 > $1.1}.first?.key ?? .dog
+        let resultedAnimal = getResultAnimal(Answers: answersChosen)
         
         titleLabel.text = "Вы - \(resultedAnimal.rawValue)"
         descriptionLabel.text = resultedAnimal.definition
+    }
+    
+    private func getResultAnimal(Answers: [Answer]) -> Animal {
+        let animals = answersChosen.map {$0.animal}
+        
+        for animal in animals {
+            if !animalsCount.keys.contains(animal) {
+                animalsCount[animal] = 1
+            } else {
+                animalsCount[animal]! += 1
+            }
+        }
+        
+        return animalsCount.sorted {$0.1 > $1.1}.first?.key ?? .dog
     }
 }
